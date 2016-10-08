5557b5ea3b0aaaddb0087b27d45c09a811b097b3
agruev <agruev@gmail.com>
2016-10-08 14:05:19 +0300

    Initial commit

---
+++ JiraRestClientTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using SiiRed;
+
+using System.Collections.Specialized;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+        
+        }
+
+        static void myConnector()
+        {
+            Console.WriteLine("Aaaaa");
+            //RedmineConnector a = new RedmineConnector();
+            //Issue[] res = new RedmineConnector().getCacheIssues("PART");
+            //foreach (Issue i in res)
+            //{
+            //    Console.WriteLine(i.subject);
+            //}
+            Console.ReadLine();
+        }
+    }
+}
